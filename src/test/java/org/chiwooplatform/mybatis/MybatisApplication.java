7e720b93208050e49d8020d44d7b9677609fbfda
seonbo.shim <seonbo.shim@SeonBoShim.hwk.net>
2017-04-28 13:42:48 +0900

    단위 테스트 클래스가 참조 하는 MybatisApplication Configuration 클래스의 컴포넌트 로더를 필요한 dependency 만 로딩 하도록 최적화 함.
    데이터베이스를 SAMPLES 에서 CHIWOOSAMPLES 으로 변경 함.

--- src/test/java/org/chiwooplatform/mybatis/MybatisApplication.java
+++ src/test/java/org/chiwooplatform/mybatis/MybatisApplication.java
@@ -1,29 +1,44 @@
 package org.chiwooplatform.mybatis;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * This class is for testing mybatis mapper.
+ */
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 import org.chiwooplatform.mybatis.dialect.MariaDBDialect;
 import org.chiwooplatform.mybatis.plugin.LoggingInterceptor;
 import org.chiwooplatform.mybatis.plugin.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 
-/**
- * This class is for testing mybatis mapper.
- */
+@SpringBootConfiguration
+@Import({ MybatisApplication.MybatisConfiguration.class })
+@ImportAutoConfiguration(classes = {
+    DataSourceAutoConfiguration.class,
+    MybatisAutoConfiguration.class,
+    DataSourceTransactionManagerAutoConfiguration.class })
 @MapperScan(basePackages = "org.chiwooplatform.sample.mapper")
-@SpringBootApplication
 public class MybatisApplication {
 
-    @Bean
-    public PaginationInterceptor paginationInterceptor() {
-        final PaginationInterceptor interceptor = new PaginationInterceptor();
-        interceptor.setDialect( new MariaDBDialect() );
-        return interceptor;
-    }
+    @Configuration
+    static class MybatisConfiguration {
 
-    @Bean
-    public LoggingInterceptor loggingInterceptor() {
-        return new LoggingInterceptor();
+        @Bean
+        public PaginationInterceptor paginationInterceptor() {
+            final PaginationInterceptor interceptor = new PaginationInterceptor();
+            interceptor.setDialect( new MariaDBDialect() );
+            return interceptor;
+        }
+
+        @Bean
+        public LoggingInterceptor loggingInterceptor() {
+            return new LoggingInterceptor();
+        }
     }
 }
